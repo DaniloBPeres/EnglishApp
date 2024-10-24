2223eaef24bf39bf3b714a700513976abf93a7f4
Danilo Peres <danilobperes2@gmail.com>
2024-10-24 14:02:44 -0300

    alts

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: {
     default: "English App",
-    template: "%s | chronark.com",
+    template: "DnZ",
   },
   description: "Instrumental English and Digital Thinking Project."
 };
--- app/project/page.tsx
+++ app/project/page.tsx
@@ -20,7 +20,7 @@ export default function ProjectPage(){
                 <div className="w-full h-px bg-zinc-400 mt-0.5 mb-6"/>
                 <div className="w-full flex flex-wrap">
                     <div className="w-1/2 mb-6 flex max-[768px]:w-full">
-                        <Image src="/iateste.jpg" width={650} height={500} alt="Ia Picture" className="rounded-xl"></Image>
+                        <Image src="/iateste.jpg" width={600} height={500} alt="Ia Picture" className="rounded-xl"></Image>
                     </div>
                     <div className="w-1/2 mb-6 max-[768px]:w-full">
                         <Card>
@@ -119,6 +119,55 @@ export default function ProjectPage(){
                                     </p>
                                 </article>
                             </Link>
+                        </Card>
+                    </div>
+                    <div className="w-full mb-6">
+                        <Card>
+                            <article className="relative w-full h-full p-4 md:p-8">
+                                <h1 className="mt-4 text-2xl font-bold text-zinc-100 group-hover:text-white font-display mb-4">
+                                    Authentication and authorization are two key concepts in digital security:
+                                </h1>
+                                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                                    Authentication: The process of verifying a user’s identity. In addition to passwords, more advanced methods include:
+                                </p>
+                                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                                    Two-Factor Authentication (2FA): Requires two different elements (such as a password and a code sent via SMS) to ensure the user is legitimate.
+                                </p>
+                                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                                    Biometrics: Uses physical characteristics, such as fingerprints or facial recognition, for authentication.
+                                </p>
+                                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                                    Authorization: Defines what an authenticated user can do within the system. Even after authentication, a user’s permissions may limit their access to certain areas or functionalities of a system.
+                                </p>
+                                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                                    Firewalls: Act as barriers that filter network traffic based on a set of security rules. They can block malicious traffic before it reaches the internal network.
+                                </p>
+                                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                                    Intrusion Detection and Prevention Systems (IDS/IPS): Tools that monitor network activity to detect attack attempts or anomalies. IDS alerts about suspicious activity, while IPS takes automatic measures to block threats.
+                                </p>
+                            </article>
+                        </Card>
+                    </div>
+                    <div className="w-full mb-6">
+                        <Card>
+                            <article className="relative w-full h-full p-4 md:p-8">
+                                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                                    Firewall and network monitoring are digital security tools and practices used to protect networks and systems from external threats and ensure the proper functioning of a digital environment. They act as barriers and control mechanisms that filter network traffic, detect suspicious activities, and prevent cyberattacks.
+                                </p>
+                                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                                    Software Security and Patches focus on software as one of the primary attack surfaces. Keeping all systems up to date with security patches is an essential practice. Developers often fix vulnerabilities as they are discovered, and failure to update software can expose systems to attacks.
+                                </p>
+                                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                                    Backups and Disaster Recovery: Regular backups ensure that critical data can be recovered after an attack or system failure. A disaster recovery plan includes strategies for quickly restoring operational systems and data, minimizing downtime.
+                                </p>
+                                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                                    Compliance and security policies are essential concepts in information security management within organizations. They ensure that a company’s operations and processes comply with standards, laws, and security guidelines, protecting data and resources from breaches and cyberattacks. Security policies, on the other hand, are the guidelines, rules, and procedures established by an organization to protect its information assets, systems, and data against threats. These policies define how data should be managed, who has access to it, how IT resources are protected, and how employees should act to ensure the company's security.
+                                    User Education and Awareness is an area that shows training users is one of the best ways to avoid attacks that exploit human error. Awareness programs teach employees how to recognize and avoid phishing attacks, protect passwords, and practice safe internet browsing.
+                                </p>
+                                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                                    Cloud Security: As more companies migrate to the cloud, new security considerations arise. Cloud service providers offer robust security, but organizations still need to ensure that their data is properly configured and protected, controlling who has access and monitoring the use of the infrastructure.
+                                </p>
+                            </article>
                         </Card>
                     </div>
                 </div>
